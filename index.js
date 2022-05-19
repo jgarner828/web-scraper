151a3a92c98a4b62a5314105c90b06ae9b537ee6
Justin Garner <j.garner828@gmail.com>
2022-05-19 12:11:47 -0400

    created basic scraper pulling article titles and hrefs into an array. current url is still hardcoded to sample site. need to find a way to add multiple sites and change search terms on demand

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
+const { find } = require('domutils');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const url = 'https://www.theguardian.com/';
+const url = 'https://www.theguardian.com/us';
 
 axios(url)
     .then(response => {
-        const html = response.data
-
+        const html = response.data;
         // taking all response data and loading into cheerio
-        const $ = cheerio.load(html)
+        const $ = cheerio.load(html);
+        const articles = [];
 
         // to locate a specific element
+        $(`.fc-item__title`, html).each(function() {
 
-    })
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href');
 
+            articles.push({
+                title,url
+            })
+        })
+
+        console.log(articles)
+         })
+    .catch(err => console.log(err))
+
+
+    
 app.listen(PORT, () => {
   console.log(`Web Scraper listening on port ${PORT}`)
 })
